87a3202df11b6f07356d474b72833e78ac1df8bb
Jean Mégret <johnsnens@gmail.com>
2023-07-31 09:20:01 +0100

    cubic_time working, before changing hybla_time

--- ccaModels.py
+++ ccaModels.py
@@ -10,28 +10,67 @@ import scipy
 import logging, sys
 
 class CCA_MarkovChain:
-    def __init__(self, N:int = 100, C:float=1000., RTT_real:float=0.025, RTT_est:float=0.025, packet_err:float=0.01, err_rate:float = 1, beta:float = 0.7):
+    def __init__(self, N:int = 100, C:float=1000., RTT_real:float=0.025, RTT_est:float=0.025, packet_err:float=0.01, err_rate:float = 1, beta:float = 0.7, alpha:float = 1.0):
         self.N = N # number of states
         self.C = C # Bottleneck bandwidth on the path (MSS/s)
         self.W = C*RTT_real # Maximum window size to avoid congestion. In MSS
         self.RTT_real = RTT_real # in s
         self.RTT_est = RTT_est # in s
-        self.packet_err = packet_err # probability that a packet drops 
-        self.err_rate = err_rate # error rate
+        self.packet_err = packet_err # probability that a packet drops (for the packet model)
+        self.err_rate = err_rate # error rate (for the time model)
         self.beta = beta # window reduction ratio
         self.a = (np.linspace(1,N,N)-0.5)*self.W/N # Discretisation of the window-size
         self.pi = np.ones(N)/N # Stationnary Distribution
         self.P = np.zeros([self.N,self.N]) # Transition Probability Matrix
         self.S = np.zeros([self.N,self.N]) 
         self.tau = np.zeros([self.N,self.N]) 
+        self.alpha = alpha # window growth rate
         self.ssThroughput = 0 # Steady State average throughput
+
+    def update(self):
+        self.W = self.C*self.RTT_real # Maximum window size to avoid congestion. In MSS
+        self.a = (np.linspace(1,self.N,self.N)-0.5)*self.W/self.N
+        self.pi = np.ones(self.N)/self.N # Stationnary Distribution
+        self.P = np.zeros([self.N,self.N]) # Transition Probability Matrix
+        self.S = np.zeros([self.N,self.N]) 
+        self.tau = np.zeros([self.N,self.N]) 
+        self.ssThroughput = 0 # Steady State average throughput
+    
+    def transition_proba(self,i:int,j:int):
+        """Pij, probability to transition from state a_i to a_j
+
+        Args:
+            i (int): initial state
+            j (int): final state
+
+        Returns:
+            float: NEED TO REDEFINE THAT FUNCTION FOR SPECIFIC MODELS
+        """
+        return 
+
+    def compute_stationnary_distribution(self):
+        # 1. Compute the transition probability Matrix P
+        for i in range(self.N):
+            for j in range(self.N):
+                if j == self.N-1:
+                    self.P[i,j] = 1-np.sum(self.P[i,:-1])
+                    continue
+                self.P[i,j] = self.transition_proba(i,j)
+        # 2. Solve the system of equation (16)&(17) 
+        # piP = pi <=> pi(P-I)=0 <=> (P-I)^T pi = 0 
+        # so pi is a left eigenvector of P, with eigenvalue 1
+        # Furthermore, pis L1 norm needs to be equal to 1 
+        w,v = np.linalg.eig(np.transpose(self.P)) # Compute eigenvalues/eigenvectors
+        self.pi = np.real(v[:,0]/v[:,0].sum()) # Scale such that the values sum to 1
+        #ws,vs = scipy.sparse.linalg.eigs(A=np.transpose(self.P),k=1,sigma=1)
+        #self.pi = np.real(vs/vs.sum())[:,0]
+        return self.pi
 
 ###----------------------CUBIC START-----------------------------###
 
 class CCA_MarkovChain_CUBIC(CCA_MarkovChain):
-    def __init__(self,alpha = 1,*args,**kwargs):
+    def __init__(self,*args,**kwargs):
         super(CCA_MarkovChain_CUBIC,self).__init__(*args,**kwargs)
-        self.alpha = alpha # window growth rate
 
     def __str__(self):
         return "CUBIC"
@@ -53,52 +92,19 @@ class CCA_MarkovChain_CUBIC(CCA_MarkovChain):
         """
         return self.alpha*(t-np.cbrt(x*(1-self.beta)/self.alpha))**3+x
     
-    def transition_proba(self,i:int,j:int):
-        """Pij, probability to transition from state a_i to a_j
-
-        Args:
-            i (int): initial state
-            j (int): final state
-
-        Returns:
-            float: NEED TO REDEFINE THAT FUNCTION FOR SPECIFIC MODELS
-        """
-        return 0
+class CCA_MarkovChain_CUBIC_time(CCA_MarkovChain_CUBIC):
     
-    def compute_stationnary_distribution(self):
-        # 1. Compute the transition probability Matrix P
-        for i in range(self.N):
-            for j in range(self.N):
-                if j == self.N:
-                    self.P[i,j] = 1-np.sum(self.P[i,:-1])
-                    continue
-                self.P[i,j] = self.transition_proba(i+1,j+1)
-
-        # 2. Solve the system of equation (16)&(17) 
-        # piP = pi <=> pi(P-I)=0 <=> (P-I)^T pi = 0 
-        # so pi is a left eigenvector of P, with eigenvalue 1
-        # Furthermore, pis L1 norm needs to be equal to 1 
-        w,v = np.linalg.eig(np.transpose(self.P)) # Compute eigenvalues/eigenvectors
-        self.pi = np.real(v[:,0]/v[:,0].sum()) # Scale such that the values sum to 1
-        # ws,vs = scipy.sparse.linalg.eigs(A=np.transpose(self.P),k=1,sigma=1)
-        # self.pi = np.real(vs/vs.sum())[:,0]
-        return self.pi
-    
-
-    
-class CCA_MarkovChain_CUBIC_OG(CCA_MarkovChain_CUBIC):
-    # Don't need to redefine the init as it is inherited from the CUBIC MarkovChain
-    def transition_proba(self,i,j): # Probability to transition from i to j
+    def transition_proba(self,i,j): # FIX INDICES SO THAT WE ALWAYS STAY WITH A_0 to A_N-1
         l = self.err_rate
-        if j < self.beta*(i-0.5):
+        if j < self.beta*(i+0.5):
             return 0
         if j == self.N:
             sum = 0
             for jp in np.linspace(1,self.N-1,self.N-1):
-                if jp >= self.beta*(i-0.5):
+                if jp >= self.beta*(i+0.5):
                     sum += self.transition_proba(i,jp)
             return 1-sum
-        return np.exp(-l* np.maximum(self.T(self.a[i-1],(j-1)*self.W/self.N),0)) - np.exp(-l* self.T(self.a[i-1],j*self.W/self.N))
+        return np.exp(-l* np.maximum(self.T(self.a[i],(j)*self.W/self.N),0)) - np.exp(-l* self.T(self.a[i],(j+1)*self.W/self.N))
     
     def compute_tau_and_S(self):
         for i in range(self.N):
@@ -113,16 +119,20 @@ class CCA_MarkovChain_CUBIC_OG(CCA_MarkovChain_CUBIC):
         self.compute_tau_and_S()
         numerator = np.dot(self.pi,np.dot(self.P,np.transpose(self.S)).diagonal())
         denominator = np.dot(self.pi,np.dot(self.P,np.transpose(self.tau)).diagonal())
-        self.ssThroughput = numerator/denominator/self.RTT_real
+        self.ssThroughput = numerator/denominator/self.W
         return self.ssThroughput
     
-class CCA_MarkovChain_CUBIC_discrete(CCA_MarkovChain_CUBIC):
+class CCA_MarkovChain_CUBIC_packet(CCA_MarkovChain_CUBIC):
     def __init__(self, *args, **kwargs):
-        super(CCA_MarkovChain_CUBIC_discrete,self).__init__(*args,**kwargs)
+        super(CCA_MarkovChain_CUBIC_packet,self).__init__(*args,**kwargs)
         # We need additional variables
         self.Dmin = np.zeros([self.N,self.N]) # Minimum distance Matrix
         self.Dmax = np.zeros([self.N,self.N]) # Maximum distance Matrix
         self.Ptilde = np.zeros((self.N,self.N)) # Shifted transition probabilities
+        self.compute_distances()
+        
+    def update(self):
+        super(CCA_MarkovChain_CUBIC_packet,self).update()
         self.compute_distances()
     
     def compute_distances(self):
@@ -203,7 +213,11 @@ class CCA_MarkovChain_Hybla(CCA_MarkovChain):
     def __init__(self, RTT0 = 0.025, *args,**kwargs):
         super(CCA_MarkovChain_Hybla,self).__init__(*args,**kwargs)
         self.RTT0 = RTT0 # target RTT
-        self.rho = max(self.RTT_real/RTT0,1)
+        self.rho = max(self.RTT_real/self.RTT0,1) # RTT ratio
+
+    def update(self):
+        super(CCA_MarkovChain_Hybla,self).update()
+        self.rho = max(self.RTT_real/self.RTT0,1)
 
     def __str__(self):
         return "HYBLA"
@@ -218,10 +232,14 @@ class CCA_MarkovChain_Hybla(CCA_MarkovChain):
         Returns:
             _type_: Time, in seconds, it takes to grow from beta*x to y.
         """
-        return self.RTT0**2*(y-self.beta*x)/self.RTT_real
+        if y <= x:
+            return 0
+        return self.RTT0/(self.alpha*self.rho)*(y-self.beta*x)
     
-    def transition_proba_Hybla(self,i,j):
-        return 0
+    def w(self,x,t):
+        """ Window size dynamics of Hybla
+        """
+        return self.alpha*self.rho*t+x
     
     def compute_stationnary_distribution(self):
         # 1. Compute the transition probability Matrix P
@@ -233,17 +251,19 @@ class CCA_MarkovChain_Hybla(CCA_MarkovChain):
                 self.P[i,j] = self.transition_proba_Hybla(i,j)
 
         # 2. Solve the system of equation (16)&(17)
-        ws,vs = scipy.sparse.linalg.eigs(A=np.transpose(self.P),k=1,sigma=1)
-        self.pi = np.real(vs/vs.sum())[:,0]
+        w,v = np.linalg.eig(np.transpose(self.P)) # Compute eigenvalues/eigenvectors
+        self.pi = np.real(v[:,0]/v[:,0].sum()) # Scale such that the values sum to 1
+        #ws,vs = scipy.sparse.linalg.eigs(A=np.transpose(self.P),k=1,sigma=1)
+        #self.pi = np.real(vs/vs.sum())[:,0]
         return 
 
-class CCA_MarkovChain_Hybla_OG(CCA_MarkovChain_Hybla):
+class CCA_MarkovChain_Hybla_time(CCA_MarkovChain_Hybla):
     
-    def transition_proba_Hybla(self,i,j):
+    def transition_proba(self,i,j):
         l = self.err_rate
         if j < self.beta*(i-0.5):
             return 0
-        if j == self.N:
+        if j == self.N-1:
             sum = 0
             for jp in np.linspace(1,self.N-1,self.N-1):
                 if jp >= self.beta*(i-0.5):
@@ -263,10 +283,10 @@ class CCA_MarkovChain_Hybla_OG(CCA_MarkovChain_Hybla):
         self.compute_tau_and_S()
         numerator = np.dot(self.pi,np.dot(self.P,np.transpose(self.S)).diagonal())
         denominator = np.dot(self.pi,np.dot(self.P,np.transpose(self.tau)).diagonal())
-        self.ssThroughput = 1/self.RTT_real*numerator/denominator
+        self.ssThroughput = 1/self.W*numerator/denominator
         return self.ssThroughput
 
-class CCA_MarkovChain_Hybla_ssd(CCA_MarkovChain_Hybla_OG):
+class CCA_MarkovChain_Hybla_ssd(CCA_MarkovChain_Hybla_time):
     def avg_w(self,t,x): # packets sent every RTT (for state-depedent lambda)
         return self.beta*x+self.rho*t/(2*self.RTT0)
     
